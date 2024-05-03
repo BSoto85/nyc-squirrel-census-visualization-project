57b40f18253d9777bd321724ccb262694679a2f9
brenda-soto <bsoto@pursuit.org>
2024-05-03 15:29:54 -0400

    Created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
+import LandingPage from "./Pages/LandingPage";
+import MapView from "./Pages/MapView";
+import ChartsView from "./Pages/ChartsView";
+import AboutTheDevs from "./Pages/AboutTheDevs";
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/map" element={<MapView />} />
+        <Route path="/charts" element={<ChartsView />} />
+        <Route path="/about" element={<AboutTheDevs />} />
       </Routes>
     </>
   );
---
+++ src/Pages/ChartsView.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartsView = () => {
+  return <div>ChartsView</div>;
+};
+
+export default ChartsView;
