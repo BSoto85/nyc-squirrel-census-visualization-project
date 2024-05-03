cbcde0a18cd266dbef5c243513d36ec9f5c1f44b
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-05-03 15:21:40 -0400

    font bigger navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <>
       <div className="bg-amber-700/90 py-6">
         <div className="flex flex-row items-center ml-5">
-          <div className="text-2xl text-zinc-100 mr-1">Squirrel Sphere</div>
+          <div className="text-4xl text-zinc-100 mr-1">Squirrel Sphere</div>
           <Squirrel size={36} className="text-zinc-100" />
           <div className="ml-auto flex justify-contents space-x-4 mr-5">
             <button className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200">
