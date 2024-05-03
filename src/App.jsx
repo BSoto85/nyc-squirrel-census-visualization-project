8ac7ebf352ee7b3c4f23f6aa55490ee0c38ed6e8
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-05-03 15:16:26 -0400

    styled navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 
 const App = () => {
   return (
     <>
-      <h1>Squirrel Census Visualization Starter</h1>
+      <NavBar />
       <Routes>
         <Route />
       </Routes>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Squirrel } from "lucide-react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <div className="bg-amber-700/90 py-6">
+        <div className="flex flex-row items-center ml-5">
+          <div className="text-2xl text-zinc-100 mr-1">Squirrel Sphere</div>
+          <Squirrel size={36} className="text-zinc-100" />
+          <div className="ml-auto flex justify-contents space-x-4 mr-5">
+            <button className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200">
+              Map
+            </button>
+            <button className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200">
+              Chart
+            </button>
+            <button className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200">
+              About
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
