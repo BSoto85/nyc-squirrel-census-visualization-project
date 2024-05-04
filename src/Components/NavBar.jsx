640436f993a2f939d898db393877401b10e87ecf
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-05-04 01:16:19 -0400

    linked navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import { Squirrel } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
       <div className="bg-amber-700/90 py-6">
         <div className="flex flex-row items-center ml-5">
-          <div className="text-4xl text-zinc-100 mr-1">Squirrel Sphere</div>
-          <Squirrel size={36} className="text-zinc-100" />
+          <Link to={"/"} className="flex items-center">
+            <div className="text-4xl text-zinc-100 mr-1">Squirrel Sphere</div>
+            <Squirrel size={36} className="text-zinc-100" />
+          </Link>
           <div className="ml-auto flex space-x-4 mr-5">
-            <button className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200">
+            <Link
+              to={"/map"}
+              className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200"
+            >
               Map
-            </button>
-            <button className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200">
+            </Link>
+            <Link
+              to={"/charts"}
+              className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200"
+            >
               Chart
-            </button>
-            <button className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200">
+            </Link>
+            <Link
+              to={"/about"}
+              className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200"
+            >
               About
-            </button>
+            </Link>
           </div>
         </div>
       </div>
