844dd3ad4dcef6860c9e79d533b1e09fec0463a3
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-05-04 00:08:22 -0400

    aboutthe devs styled + linked

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         <div className="flex flex-row items-center ml-5">
           <div className="text-4xl text-zinc-100 mr-1">Squirrel Sphere</div>
           <Squirrel size={36} className="text-zinc-100" />
-          <div className="ml-auto flex justify-contents space-x-4 mr-5">
+          <div className="ml-auto flex space-x-4 mr-5">
             <button className="p-3 bg-yellow-50 rounded-2xl hover:bg-yellow-200">
               Map
             </button>
